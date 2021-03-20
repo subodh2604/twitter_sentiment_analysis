ce904a6c9d6e3f121864d98cdb5d31deee6c516f
Subodh Halpatrao <subodhhalpatrao777@gmail.com>
2021-03-20 19:01:30 +0530

    first commit

---
+++ sentiment_analysis/emotion/tweepy_emotion.py
@@ -0,0 +1,47 @@
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
+import tweepy
+import numpy as np
+import pandas as pd
+
+class Import_tweet_emotion:
+
+	consumer_key="AHiSZ556sAEQkO5hL2EdQ41IR"
+	consumer_secret="waDZHfxQYNZuZDbXAM9k900bal3Tx3W9fGkPHJ7Env02hcHj1B"
+	access_token="1372164636062085121-RtfXSGuacPfQQIWLFLLuDcbElzub1l"
+	access_token_secret="weiEDse4of7IpUxBVwNNPoCMXzHO03VQAftbY34awn3Jp"
+
+	def tweet_to_data_frame(self, tweets):
+		df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+		return df
+
+	def get_tweets(self, handle):
+		auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+		auth.set_access_token(self.access_token, self.access_token_secret)
+		auth_api = API(auth)
+
+		account = handle
+		item = auth_api.user_timeline(id=account,count=20)
+		df = self.tweet_to_data_frame(item)
+
+		all_tweets = []
+		for j in range(20):
+			all_tweets.append(df.loc[j]['Tweets'])
+		return all_tweets
+
+	def get_hashtag(self, hashtag):
+		auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+		auth.set_access_token(self.access_token, self.access_token_secret)
+		auth_api = API(auth)
+
+		account = hashtag
+		all_tweets = []
+
+		for tweet in tweepy.Cursor(auth_api.search, q=account, lang='en').items(20):
+			all_tweets.append(tweet.text)
+
+		return all_tweets
